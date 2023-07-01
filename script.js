4e408d3bb23b363ed867505d2d2261549320a0b3
Cleven Brown <hinokamikagustuchi@gmail.com>
2023-06-30 21:24:25 -0400

    basic outline complete

--- script.js
+++ script.js
@@ -14,7 +14,9 @@ const rooms = [
     title: "Flooded Elevator",
     description: "you choose to enter the elevator and find that there are only two buttons one for up and one for down you contemplate on your decision until you finally decide the doors start closing until there is no more light that reaches you.",
     image: "images/dark elevator.jpeg",
-    choices: [{ text: "Go back to front room", nextRoom: "start" }],
+    choices: [
+      { text: "go down", nextRoom: "flooded" }
+    ],
   },
   {
     id: "stairs",
@@ -23,16 +25,30 @@ const rooms = [
     image: "images/stairs.jpeg",
     choices: [
       { text: "Go to elevator", nextRoom: "elevator" },
-      { text: "Go to front room", nextRoom: "start" },
+      { text: "Go down the steps", nextRoom: "stairs2" },
     ],
   },
   {
     id: "flooded",
     title: "Flooded ",
-    description: "you choose to enter the elevator and find that there are only two buttons one for up and one for down you contemplate on your decision until you finally decide the doors start closing until there is no more light that reaches you.",
-    image: "images/dark elevator.jpeg",
-    choices: [{ text: "Go back to front room", nextRoom: "start" }],
+    description: "you died",
+    image: "images/ezgif.com-webp-to-jpg.jpg",
+    choices: [{ text: "start over", nextRoom: "start" }],
   },
+  {
+    id: "stairs2",
+    title: "the bottom ",
+    description: "you chose to go down the steps and surprisingly nothing out of the ordinary happend though you should really do some cardio you should not not be that out of breath",
+    image: "images/images.jpg",
+    choices: [{ text: "go home", nextRoom: "freedom" }],
+  },
+  {
+    id: "freedom",
+    title: "freedom ",
+    description: " yay hippe you made it home enjoy to enjoy your food and your bed",
+    image: "images/download.jpg",
+    choices: [{ text: "start over", nextRoom: "start" }],
+  }
 ];
 
 const roomTitle = document.getElementById("room-title");
